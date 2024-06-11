2033f91a42701ec7e0443211f3dc27651153a2be
halatsWol <halatschek.wolfram@gmail.com>
2024-06-11 07:02:49 +0200

    removed TestFiles

--- py/wsTest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-CALL "C:\ProgramData\Marflow Software\SmartCut\py\SmartCut_Venv\Scripts\activate.bat"
-CALL "cd /d D:\OneDrive\Documents\MarflowSoftware\SmartCut"
-python "wsTest.py"
-CALL "C:\ProgramData\Marflow Software\SmartCut\pySmartCut_Venv\Scripts\deactivate.bat"
